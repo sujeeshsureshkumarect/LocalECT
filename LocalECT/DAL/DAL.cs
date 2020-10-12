db3d0ac22740b91bfab7d51445a9249bf271c874
sujeesh.sureshkumar <sujeesh.sureshkumar@ect.ac.ae>
2020-10-12 09:53:43 +0400

    Add project files.

---
+++ LocalECT/DAL/DAL.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LocalECT.DAL
+{
+    public class DAL
+    {
+        public DataTable GetMenuData()
+        {
+            DataTable dt = GetData("SELECT O.ObjectID, O.ObjectNameEn, O.DisplayObjectName, O.ShowOrder, O.SystemID, O.ParentID, O.sURL, O.iLevel, COUNT(RP.PrivilegeID) AS [Privileges] FROM Cmn_RolePermissions AS RP INNER JOIN Cmn_PrivilegeObjects AS O ON RP.ObjectID = O.ObjectID Where RP.RoleID = 165 and O.SystemID = 10 GROUP BY O.ObjectID, O.ObjectNameEn, O.DisplayObjectName, O.ShowOrder, O.SystemID, O.ParentID, O.sURL, O.iLevel Order by O.iLevel,O.ShowOrder asc");
+            return dt;
+        }
+        public DataTable GetStudentServices()
+        {
+            DataTable dt = GetData("SELECT     S.ServiceID, S.ServiceEn, S.ServiceAr, S.ServiceDescEn, S.ServiceDescAr, S.Audience, S.Host, S.HostDesc, S.FeesType, S.RequestList, S.RequestLink, S.ExampleLink, ISNULL(F.curAmount, 0) AS Fees, ISNULL(F.curVAT, 0) AS VAT,ISNULL((F.curAmount + F.curVAT),0) as Sum, S.DataNeeded FROM         ECT_Services AS S LEFT OUTER JOIN (SELECT     bytePaymentFor, curAmount, curVAT FROM          ECTData.dbo.Acc_Payment_For AS ACCP) AS F ON S.FeesType = F.bytePaymentFor where S.Audience='Students' ORDER BY S.ServiceID asc");
+            return dt;
+        }
+        public DataTable GetStudentServicesbyID(string serviceid)
+        {
+            DataTable dt = GetData("SELECT     S.Finance,S.ServiceID, S.ServiceEn, S.ServiceAr, S.ServiceDescEn, S.ServiceDescAr, S.Audience, S.Host, S.HostDesc, S.FeesType, S.RequestList, S.RequestLink, S.ExampleLink, ISNULL(F.curAmount, 0) AS Fees, ISNULL(F.curVAT, 0) AS VAT,ISNULL((F.curAmount + F.curVAT),0) as Sum, S.DataNeeded FROM         ECT_Services AS S LEFT OUTER JOIN (SELECT     bytePaymentFor, curAmount, curVAT FROM          ECTData.dbo.Acc_Payment_For AS ACCP) AS F ON S.FeesType = F.bytePaymentFor where S.Audience='Students' and S.ServiceID='" + serviceid + "' ORDER BY S.ServiceID asc");
+            return dt;
+        }
+
+        public DataTable GetStudentDetailsID(string studentid, string connStr)
+        {
+            DataTable dt = GetData("SELECT        Reg_Applications.lngStudentNumber, Reg_Students_Data.strLastDescEn, Reg_Specializations.strCaption, dbo.cleanphone(Reg_Students_Data.strPhone1) AS Phone, Reg_Students_Data.sECTemail,Reg_Applications.byteCancelReason,Reg_Students_Data.strNationalID AS EID FROM Reg_Applications INNER JOIN Reg_Students_Data ON Reg_Applications.lngSerial = Reg_Students_Data.lngSerial INNER JOIN Reg_Specializations ON Reg_Applications.strCollege = Reg_Specializations.strCollege AND Reg_Applications.strDegree = Reg_Specializations.strDegree AND Reg_Applications.strSpecialization = Reg_Specializations.strSpecialization where Reg_Applications.lngStudentNumber='" + studentid + "'", connStr);
+            return dt;
+        }
+
+        public DataTable GetStudentDetailsIDbyNationality(string studentid, string connStr)
+        {
+            DataTable dt = GetData("SELECT Reg_Applications.lngStudentNumber, Reg_Students_Data.strLastDescEn, Reg_Specializations.strCaption, dbo.CleanPhone(Reg_Students_Data.strPhone1) AS Phone, Reg_Students_Data.sECTemail, Reg_Applications.byteCancelReason,Reg_Applications.intGraduationYear,Reg_Applications.byteGraduationSemester,  Reg_Students_Data.strNationalID AS EID, Lkp_Nationalities.strNationalityDescEn, Reg_Students_Data.dateBirth FROM Reg_Applications INNER JOIN Reg_Students_Data ON Reg_Applications.lngSerial = Reg_Students_Data.lngSerial INNER JOIN Reg_Specializations ON Reg_Applications.strCollege = Reg_Specializations.strCollege AND Reg_Applications.strDegree = Reg_Specializations.strDegree AND Reg_Applications.strSpecialization = Reg_Specializations.strSpecialization INNER JOIN Lkp_Nationalities ON Reg_Students_Data.byteNationality = Lkp_Nationalities.byteNationality where Reg_Applications.lngStudentNumber='" + studentid + "'", connStr);
+            return dt;
+        }
+
+        public DataTable GetStudentDetailsForProfile(string studentid, string connStr)
+        {
+            DataTable dt = GetData("SELECT Reg_Applications.lngStudentNumber, Reg_Students_Data.strLastDescEn, Reg_Specializations.strCaption, dbo.CleanPhone(Reg_Students_Data.strPhone1) AS Phone, Reg_Students_Data.sECTemail, Reg_Applications.byteCancelReason,Reg_Applications.intGraduationYear,Reg_Applications.byteGraduationSemester,  Reg_Students_Data.strNationalID, Lkp_Nationalities.strNationalityDescEn, Reg_Students_Data.dateBirth,Reg_Students_Data.lngSerial, Reg_Students_Data.lngStudentNumber, Reg_Students_Data.iUnifiedID, Reg_Students_Data.strStudentPic  FROM Reg_Applications INNER JOIN Reg_Students_Data ON Reg_Applications.lngSerial = Reg_Students_Data.lngSerial INNER JOIN Reg_Specializations ON Reg_Applications.strCollege = Reg_Specializations.strCollege AND Reg_Applications.strDegree = Reg_Specializations.strDegree AND Reg_Applications.strSpecialization = Reg_Specializations.strSpecialization INNER JOIN Lkp_Nationalities ON Reg_Students_Data.byteNationality = Lkp_Nationalities.byteNationality where Reg_Applications.lngStudentNumber='" + studentid + "'", connStr);
+            return dt;
+        }
+
+        public DataTable GetCoursesbyStudentId(string studentid, string connStr, int year, int sem)
+        {
+            DataTable dt = GetData("SELECT  Course_Balance_View_BothSides.iYear, Course_Balance_View_BothSides.Sem, Course_Balance_View_BothSides.Student, Course_Balance_View_BothSides.Course AS Code, Course_Balance_View_BothSides.Course + N'-' + Reg_Courses.strCourseDescEn AS Course, Reg_Lecturers.strLecturerDescEn FROM            Course_Balance_View_BothSides INNER JOIN Reg_Courses ON Course_Balance_View_BothSides.Course = Reg_Courses.strCourse INNER JOIN Reg_CourseTime_Schedule ON Course_Balance_View_BothSides.iYear = Reg_CourseTime_Schedule.intStudyYear AND Course_Balance_View_BothSides.Sem = Reg_CourseTime_Schedule.byteSemester AND Course_Balance_View_BothSides.Course = Reg_CourseTime_Schedule.strCourse INNER JOIN Reg_Lecturers ON Reg_CourseTime_Schedule.intLecturer = Reg_Lecturers.intLecturer WHERE        (Course_Balance_View_BothSides.iYear = " + year + ") AND (Course_Balance_View_BothSides.Sem = " + sem + ") AND (Course_Balance_View_BothSides.Student = N'" + studentid + "') AND (Reg_CourseTime_Schedule.bLab = 0)", connStr);
+            return dt;
+        }
+
+        public DataTable GetSemestersbyTerm(int year, int sem)
+        {
+            int term = (year * 10) + sem;
+            DataTable dt = GetData("SELECT  top 4   AcademicYear, Semester, LongDesc, AcademicYear * 10 + Semester AS Term FROM Reg_Semester where term<" + term + " ORDER BY AcademicYear DESC, Semester DESC");
+            return dt;
+        }
+
+        public DataTable GetCoursesbyCourseId(string studentid, string connStr, int year, int sem, string courseid)
+        {
+            DataTable dt = GetData("SELECT  Course_Balance_View_BothSides.iYear, Course_Balance_View_BothSides.Sem, Course_Balance_View_BothSides.Student, Course_Balance_View_BothSides.Course AS Code, Course_Balance_View_BothSides.Course + N'-' + Reg_Courses.strCourseDescEn AS Course, Reg_Lecturers.strLecturerDescEn FROM            Course_Balance_View_BothSides INNER JOIN Reg_Courses ON Course_Balance_View_BothSides.Course = Reg_Courses.strCourse INNER JOIN Reg_CourseTime_Schedule ON Course_Balance_View_BothSides.iYear = Reg_CourseTime_Schedule.intStudyYear AND Course_Balance_View_BothSides.Sem = Reg_CourseTime_Schedule.byteSemester AND Course_Balance_View_BothSides.Course = Reg_CourseTime_Schedule.strCourse INNER JOIN Reg_Lecturers ON Reg_CourseTime_Schedule.intLecturer = Reg_Lecturers.intLecturer WHERE        (Course_Balance_View_BothSides.iYear = " + year + ") AND (Course_Balance_View_BothSides.Sem = " + sem + ") AND (Course_Balance_View_BothSides.Student = N'" + studentid + "') AND (Reg_CourseTime_Schedule.bLab = 0) AND Course_Balance_View_BothSides.Course='" + courseid + "' ", connStr);
+            return dt;
+        }
+
+        public DataTable GetMajors(string connStr)
+        {
+            DataTable dt = GetData("SELECT distinct(strCaption) FROM Reg_Specializations WHERE (bAvailable = 1) and strCaption not in ('Diploma in Public Relations','Visiting Students') ORDER BY strCaption asc", connStr);
+            return dt;
+        }
+
+        public DataTable GetStudentAcademicAdvisor(string studentid, string connStr)
+        {
+            DataTable dt = GetData("SELECT Reg_Applications.lngStudentNumber, Reg_Lecturers.strLecturerDescEn FROM Reg_Applications LEFT OUTER JOIN Reg_Lecturers ON Reg_Applications.intAdvisor = Reg_Lecturers.intLecturer WHERE Reg_Applications.lngStudentNumber='" + studentid + "'", connStr);
+            return dt;
+        }
+
+        public DataTable GetMainReasons()
+        {
+            DataTable dt = GetData("SELECT [byteMainReason],[strMainReasonEn],[strMainReasonAr], ISNULL([strMainReasonEn],'') + ' (' + ISNULL([strMainReasonAr],'')+' )' as MainReason FROM [ECTData].[dbo].[Lkp_MainReasons] where byteMainReason !=0");
+            return dt;
+        }
+
+        public DataTable GetSubReasons(int MainReasonId)
+        {
+            DataTable dt = GetData("SELECT byteMainReason, byteSubReson,ISNULL(strSubReasonEn,'') + ' (' + ISNULL(strSubReasonAr,'')+' )' as SubReason FROM Lkp_SubReasons where byteSubReson!=0 and byteMainReason=" + MainReasonId + "");
+            return dt;
+        }
+
+        public DataTable GetData(string query)
+        {
+            DataTable dt = new DataTable();
+            string constr = ConfigurationManager.ConnectionStrings["ECTDataNew"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        sda.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+
+        public DataTable GetData(string query, string connStr)
+        {
+            DataTable dt = new DataTable();
+            string constr = connStr;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        sda.Fill(dt);
+                    }
+                }
+                return dt;
+            }
+        }
+    }
+
+}
---
+++ LocalECT/LocalECT.Master.cs
@@ -0,0 +1,116 @@
+﻿using LocalECT.DAL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LocalECT
+{
+    public partial class LocalECT : System.Web.UI.MasterPage
+    {
+        SqlConnection sc = new SqlConnection(ConfigurationManager.ConnectionStrings["ECTDataNew"].ConnectionString);
+        DataTable Menus = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Generate_Menu();
+            }
+        }
+        public void Generate_Menu()
+        {
+            var Menu = new DAL.DAL();
+            Menus = Menu.GetMenuData();
+            DataView view = new DataView(Menus);
+            view.RowFilter = "iLevel=0";
+            this.rptCategories.DataSource = view;
+            this.rptCategories.DataBind();
+        }
+        protected void rptCategories_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+                {
+                    if (Menus != null)
+                    {
+                        DataRowView drv = e.Item.DataItem as DataRowView;
+                        string ID = drv["ObjectID"].ToString();
+                        string Title = drv["DisplayObjectName"].ToString();
+                        DataRow[] rows = Menus.Select("ParentID='" + ID + "' AND iLevel <> 0");
+                        // DataRow[] rows = Menus.Select("ParentID=" + ID);
+                        if (rows.Length > 0)
+                        {
+
+                            StringBuilder sb = new StringBuilder();
+                            sb.Append("<ul class='nav child_menu'>");
+                            foreach (var item in rows)
+                            {
+                                string parentId = item["ObjectID"].ToString();
+                                string parentTitle = item["DisplayObjectName"].ToString();
+
+                                DataRow[] parentRow = Menus.Select("ParentID=" + parentId);
+
+                                if (parentRow.Count() > 0)
+                                {
+                                    sb.Append("<li><a>" + item["DisplayObjectName"] + "<span class='fa fa-chevron-down'></span></a>");
+                                    //sb.Append("</li>");
+                                }
+                                else
+                                {
+                                    sb.Append("<li><a href='" + item["sURL"] + "'>" + item["DisplayObjectName"] + "</a>");
+                                    //sb.Append("</li>");
+                                }
+                                sb = CreateChild(sb, parentId, parentTitle, parentRow);
+                            }
+                            sb.Append("</li>");
+                            sb.Append("</ul>");
+                            (e.Item.FindControl("ltrlSubMenu") as Literal).Text = sb.ToString();
+                        }
+                    }
+                }
+            }
+        }
+        private StringBuilder CreateChild(StringBuilder sb, string parentId, string parentTitle, DataRow[] parentRows)
+        {
+            if (parentRows.Length > 0)
+            {
+                //sb.Append("<ul class='nav child_menu'>");sub_menu
+                sb.Append("<ul class='nav child_menu'>");
+                foreach (var item in parentRows)
+                {
+                    string childId = item["ObjectID"].ToString();
+                    string childTitle = item["DisplayObjectName"].ToString();
+                    DataRow[] childRow = Menus.Select("ParentID=" + childId);
+
+                    if (childRow.Count() > 0)
+                    {
+                        sb.Append("<li><a>" + item["DisplayObjectName"] + "<span class='fa fa-chevron-down'></span></a>");
+
+                        //sb.Append("</li>");
+                    }
+                    else
+                    {
+                        sb.Append("<li  class='sub_menu'><a href='" + item["sURL"] + "'>" + item["DisplayObjectName"] + "</a>");
+                        // sb.Append("</li>");
+                    }
+                    CreateChild(sb, childId, childTitle, childRow);
+                }
+                sb.Append("</li>");
+                sb.Append("</ul>");
+            }
+            return sb;
+        }
+
+        protected void lnk_Logout_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login");
+        }
+    }
+}
