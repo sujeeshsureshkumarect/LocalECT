fb807ba6e93bd958bb306d98193503c95fb280d6
sujeesh.sureshkumar <sujeesh.sureshkumar@ect.ac.ae>
2021-06-09 11:28:18 +0400

    Published

--- LocalECT/Graduates_Tracking.aspx.cs
+++ LocalECT/Graduates_Tracking.aspx.cs
@@ -121,26 +121,44 @@ namespace LocalECT
       SqlConnection sc = new SqlConnection(myConnection_String.Conn_string);
       string sSQL = "";
 
-      sSQL = "SELECT   A.intStudyYear AS InYear, A.intStudyYear * 10 + A.byteSemester AS InTerm, FTRMajor.FTR, dbo.GetStType(A.intStudyYear * 10 + A.byteSemester, A.intStudyYear * 10 + A.byteSemester, R.RIn, R.RStatus) AS STType,  ";
-      sSQL += " '"+GenderExt+"' + CONVERT(VARCHAR, SD.iUnifiedID) AS UID,A.lngStudentNumber AS SID, SD.strLastDescEn AS Name, '"+Gender+"' AS Gender, H.sngGrade AS HS, E.strCert AS ENG, E.Mark AS Score, G.GPA AS CGPA, N.strNationalityDescEn AS Nationality, M.strCaption AS InMajor,  ";
-      sSQL += "  A.intGraduationYear AS OYear, A.intGraduationYear * 10 + A.byteGraduationSemester AS OTerm, S.strReasonDesc AS OStatus, LT.MaxYear AS LTR, CM.strCaption AS CMajor, R.Ref, R.RIn, R.RMajor, R.ROut, R.RStatus  ";
-      sSQL += "FROM  Lkp_Nationalities AS N INNER JOIN  Reg_Students_Data AS SD INNER JOIN  Reg_Applications AS A ON SD.lngSerial = A.lngSerial ON N.byteNationality = SD.byteNationality INNER JOIN  ";
-      sSQL += " Reg_Specializations AS CM ON A.strCollege = CM.strCollege AND A.strDegree = CM.strDegree AND A.strSpecialization = CM.strSpecialization INNER JOIN  ";
-      sSQL += "  Last_Time_Registered AS LT ON A.lngStudentNumber = LT.Student INNER JOIN  FTRMajor ON A.lngStudentNumber = FTRMajor.Student INNER JOIN  ";
-      sSQL += "  Reg_Specializations AS M ON FTRMajor.strDegree = M.strDegree AND FTRMajor.strMajor = M.strSpecialization LEFT OUTER JOIN  ";
-      sSQL += "  MaxEngCertMark AS E ON A.lngStudentNumber = E.lngStudentNumber LEFT OUTER JOIN  ";
-      sSQL += "  GPA_Until AS G ON A.lngStudentNumber = G.lngStudentNumber LEFT OUTER JOIN  ";
-      sSQL += "  HSchools AS H ON SD.lngSerial = H.lngSerial LEFT OUTER JOIN  ";
-      sSQL += "   (SELECT        A0.lngStudentNumber AS Ref, A0.intStudyYear * 10 + A0.byteSemester AS RIn, M0.strCaption AS RMajor, A0.intGraduationYear * 10 + A0.byteGraduationSemester AS ROut, S0.strReasonDesc AS RStatus  ";
-      sSQL += "   FROM  Reg_Applications AS A0 INNER JOIN  ";
-      sSQL += "   Reg_Specializations AS M0 ON A0.strCollege = M0.strCollege AND A0.strDegree = M0.strDegree AND A0.strSpecialization = M0.strSpecialization INNER JOIN  ";
-      sSQL += "  Lkp_Reasons AS S0 ON A0.byteCancelReason = S0.byteReason) AS R ON A.sReference = R.Ref LEFT OUTER JOIN  ";
-      sSQL += "       Lkp_Reasons AS S ON A.byteCancelReason = S.byteReason  ";
-      sSQL += "WHERE        (A.byteCancelReason = 3 OR  ";
-      sSQL += "   A.byteCancelReason = 25) AND (A.intGraduationYear * 10 + A.byteGraduationSemester BETWEEN "+ddlRegTermFrom.SelectedValue+" AND "+ddlRegTermTo.SelectedValue+") AND (FTRMajor.strDegree = '1' OR  ";
-      sSQL += "            FTRMajor.strDegree = '3') AND (FTRMajor.strMajor <> '999') "+Byshifts+"  ";
-      
-     
+            //sSQL = "SELECT   A.intStudyYear AS InYear, A.intStudyYear * 10 + A.byteSemester AS InTerm, FTRMajor.FTR, dbo.GetStType(A.intStudyYear * 10 + A.byteSemester, A.intStudyYear * 10 + A.byteSemester, R.RIn, R.RStatus) AS STType,  ";
+            //sSQL += " '" + GenderExt + "' + CONVERT(VARCHAR, SD.iUnifiedID) AS UID,A.lngStudentNumber AS SID, SD.strLastDescEn AS Name, '" + Gender + "' AS Gender, H.sngGrade AS HS, E.strCert AS ENG, E.Mark AS Score, G.GPA AS CGPA, N.strNationalityDescEn AS Nationality, M.strCaption AS InMajor,  ";
+            //sSQL += "  A.intGraduationYear AS OYear, A.intGraduationYear * 10 + A.byteGraduationSemester AS OTerm, S.strReasonDesc AS OStatus, LT.MaxYear AS LTR, CM.strCaption AS CMajor, R.Ref, R.RIn, R.RMajor, R.ROut, R.RStatus  ";
+            //sSQL += "FROM  Lkp_Nationalities AS N INNER JOIN  Reg_Students_Data AS SD INNER JOIN  Reg_Applications AS A ON SD.lngSerial = A.lngSerial ON N.byteNationality = SD.byteNationality INNER JOIN  ";
+            //sSQL += " Reg_Specializations AS CM ON A.strCollege = CM.strCollege AND A.strDegree = CM.strDegree AND A.strSpecialization = CM.strSpecialization INNER JOIN  ";
+            //sSQL += "  Last_Time_Registered AS LT ON A.lngStudentNumber = LT.Student INNER JOIN  FTRMajor ON A.lngStudentNumber = FTRMajor.Student INNER JOIN  ";
+            //sSQL += "  Reg_Specializations AS M ON FTRMajor.strDegree = M.strDegree AND FTRMajor.strMajor = M.strSpecialization LEFT OUTER JOIN  ";
+            //sSQL += "  MaxEngCertMark AS E ON A.lngStudentNumber = E.lngStudentNumber LEFT OUTER JOIN  ";
+            //sSQL += "  GPA_Until AS G ON A.lngStudentNumber = G.lngStudentNumber LEFT OUTER JOIN  ";
+            //sSQL += "  HSchools AS H ON SD.lngSerial = H.lngSerial LEFT OUTER JOIN  ";
+            //sSQL += "   (SELECT        A0.lngStudentNumber AS Ref, A0.intStudyYear * 10 + A0.byteSemester AS RIn, M0.strCaption AS RMajor, A0.intGraduationYear * 10 + A0.byteGraduationSemester AS ROut, S0.strReasonDesc AS RStatus  ";
+            //sSQL += "   FROM  Reg_Applications AS A0 INNER JOIN  ";
+            //sSQL += "   Reg_Specializations AS M0 ON A0.strCollege = M0.strCollege AND A0.strDegree = M0.strDegree AND A0.strSpecialization = M0.strSpecialization INNER JOIN  ";
+            //sSQL += "  Lkp_Reasons AS S0 ON A0.byteCancelReason = S0.byteReason) AS R ON A.sReference = R.Ref LEFT OUTER JOIN  ";
+            //sSQL += "       Lkp_Reasons AS S ON A.byteCancelReason = S.byteReason  ";
+            //sSQL += "WHERE        (A.byteCancelReason = 3 OR  ";
+            //sSQL += "   A.byteCancelReason = 25) AND (A.intGraduationYear * 10 + A.byteGraduationSemester BETWEEN " + ddlRegTermFrom.SelectedValue + " AND " + ddlRegTermTo.SelectedValue + ") AND (FTRMajor.strDegree = '1' OR  ";
+            //sSQL += "            FTRMajor.strDegree = '3') AND (FTRMajor.strMajor <> '999') " + Byshifts + "  ";
+
+            sSQL  = " SELECT        A.intStudyYear AS InYear, A.intStudyYear * 10 + A.byteSemester AS InTerm, dbo.GetStType(A.intStudyYear * 10 + A.byteSemester, A.intStudyYear * 10 + A.byteSemester, R.RIn, R.RStatus) AS STType, '" + GenderExt + "' + CONVERT(VARCHAR,  ";
+            sSQL += "                          SD.iUnifiedID) AS UID, A.lngStudentNumber AS SID, SD.strLastDescEn AS Name, '" + Gender + "' AS Gender, H.sngGrade AS HS, E.strCert AS ENG, E.Mark AS Score, G.GPA AS CGPA, N.strNationalityDescEn AS Nationality,  ";
+            sSQL += "                          A.intGraduationYear AS OYear, A.intGraduationYear * 10 + A.byteGraduationSemester AS OTerm, S.strReasonDesc AS OStatus, LT.MaxYear AS LTR, CM.strCaption AS CMajor, R.Ref, R.RIn, R.RMajor, R.ROut, R.RStatus  ";
+            sSQL += " FROM            MaxEngCertMark AS E RIGHT OUTER JOIN  ";
+            sSQL += "                          Lkp_Nationalities AS N INNER JOIN  ";
+            sSQL += "                          Reg_Students_Data AS SD INNER JOIN  ";
+            sSQL += "                          Reg_Applications AS A ON SD.lngSerial = A.lngSerial ON N.byteNationality = SD.byteNationality INNER JOIN  ";
+            sSQL += "                          Reg_Specializations AS CM ON A.strCollege = CM.strCollege AND A.strDegree = CM.strDegree AND A.strSpecialization = CM.strSpecialization INNER JOIN  ";
+            sSQL += "                          Last_Time_Registered AS LT ON A.lngStudentNumber = LT.Student ON E.lngStudentNumber = A.lngStudentNumber LEFT OUTER JOIN  ";
+            sSQL += "                          GPA_Until AS G ON A.lngStudentNumber = G.lngStudentNumber LEFT OUTER JOIN  ";
+            sSQL += "                          HSchools AS H ON SD.lngSerial = H.lngSerial LEFT OUTER JOIN  ";
+            sSQL += "                              (SELECT        A0.lngStudentNumber AS Ref, A0.intStudyYear * 10 + A0.byteSemester AS RIn, M0.strCaption AS RMajor, A0.intGraduationYear * 10 + A0.byteGraduationSemester AS ROut, S0.strReasonDesc AS RStatus  ";
+            sSQL += "                                FROM            Reg_Applications AS A0 INNER JOIN  ";
+            sSQL += "                                                          Reg_Specializations AS M0 ON A0.strCollege = M0.strCollege AND A0.strDegree = M0.strDegree AND A0.strSpecialization = M0.strSpecialization INNER JOIN  ";
+            sSQL += "                                                          Lkp_Reasons AS S0 ON A0.byteCancelReason = S0.byteReason) AS R ON A.sReference = R.Ref LEFT OUTER JOIN  ";
+            sSQL += "                          Lkp_Reasons AS S ON A.byteCancelReason = S.byteReason  ";
+            sSQL += " WHERE        (A.byteCancelReason = 3 or A.byteCancelReason = 25) AND (A.intGraduationYear * 10 + A.byteGraduationSemester BETWEEN " + ddlRegTermFrom.SelectedValue + " AND " + ddlRegTermTo.SelectedValue + ") " + Byshifts + "  ";
+            sSQL += " AND (A.strDegree = N'1' OR  ";
+            sSQL += "                          A.strDegree = N'3')  ";
 
       SqlCommand cmd1 = new SqlCommand(sSQL, sc);
       cmd1.CommandTimeout = 180;
